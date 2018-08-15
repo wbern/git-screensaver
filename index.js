53db93e1e0e216a22f368d6a2df7cb8ecdff1dd6
William Bernting <william.bernting@kits.se>
2018-08-15 12:50:01 +0200

    refined options via args

--- index.js
+++ index.js
@@ -2,16 +2,48 @@ const process = require('process');
 const path = require('path');
 const fs = require('fs');
 const os = require('os');
-const moment = require('moment');
 const tensify = require('tensify');
 const whimsy = require('whimsy');
 const args = require('args');
+const micromustache = require('micromustache');
+
+const moment = require('moment');
+// require('moment/locale/sv');
+// moment.locale('sv');
 
 const createGit = require('simple-git/promise');
 
-const $USER = os.userInfo().username;
-
-args.option('whimsy', 'Add some whimsiness', false);
+args.option('hide-merges', 'Do not show commits starting with [Mm]erge', true);
+args.option(
+  'fetch',
+  'Should we fetch on all repos before doing git log?',
+  true
+);
+args.option(
+  'commit-text',
+  'How to display the commit text line',
+  '{{relativeTime}}: {{message}}'
+);
+args.option('since', 'How far back to display commits.', '1 month ago');
+args.option('author', 'Author(s) to filter by.', os.userInfo().username);
+args.option('show-all-authors', 'Show all authors', false);
+args.option(
+  'clear-screen',
+  'Clear screen instead of scrolling down (good when you get slowness)',
+  false
+);
+args.option('scroll-text', 'Text to output when scrolling the screen', '.');
+args.option('scroll-amount', 'Lines to scroll', 50);
+args.option(
+  'banner-text',
+  "Text to display, eg: 'I have committed in...'",
+  'I have committed in...'
+);
+args.option(
+  'banner-separator-text',
+  "Text to display after the banner text, eg. '--------'",
+  '------------'
+);
 
 const flags = args.parse(process.argv);
 
@@ -30,6 +62,18 @@ function getGitEnabledDirectories(directories) {
   return directories.filter(dir => fs.existsSync(path.join(dir, '.git')));
 }
 
+let gitLogOptions = {
+  '--all': null
+};
+
+if (flags.author && !flags.showAllAuthors) {
+  gitLogOptions['--author'] = flags.author;
+}
+
+if (flags.since) {
+  gitLogOptions['--since'] = flags.since;
+}
+
 let promises = getGitEnabledDirectories(
   getDirectories(path.resolve(path.join(__dirname, '../../Repos')))
 ).map(gitDir => {
@@ -37,28 +81,24 @@ let promises = getGitEnabledDirectories(
   git.silent(true);
 
   return new Promise(resolve => {
-    // git
-    // .fetch()
-    // .then(() =>
-    git
-      .log({
-        // '--since': 'last week',
-        '--author': $USER,
-        '--all': null
-      })
-      // )
-      .then(result => {
-        return {
-          result,
-          gitDir
-        };
-      })
-      .then(r => {
-        resolve(r);
-      })
-      .catch(() => {
-        resolve();
-      });
+    let gitLog = () =>
+      git
+        .log(gitLogOptions)
+        .then(result => {
+          return {
+            result,
+            gitDir
+          };
+        })
+        .then(r => {
+          resolve(r);
+        })
+        .catch(() => {
+          resolve();
+        });
+
+    // either fetch first, or do git-log immediately
+    flags.fetch ? git.fetch().then(gitLog) : gitLog();
   });
 });
 
@@ -97,52 +137,60 @@ function scroll(newlineSymbol = '.', padAmount = 7, marginAmount = 0) {
   }
 }
 
+function clearScreen() {
+  // ANSI special character for VT100 terminals (aka phosphor screensaver)
+  process.stdout.write('\x1Bc');
+}
+
 // eslint-disable-next-line no-new
 new Promise(resolve => {
   // banner message
-  log('Today I have committed in...');
-  log('------------');
+  log(flags.bannerText);
+  log(flags.bannerSeparatorText);
 
   Promise.all(promises).then(commands => {
     commands.forEach(command => {
       if (command && command.result.all.length > 0) {
         // message above each repo
         log('');
-        log(
-          path.basename(command.gitDir) + ':'
+        log(path.basename(command.gitDir) + ':');
+
+        let commits = command.result.all.filter(
+          commit =>
+            !flags.hideMerges ||
+            (commit.message &&
+              !commit.message.toLowerCase().startsWith('merge'))
         );
 
-        command.result.all.forEach(lineEntry => {
+        commits.forEach(lineEntry => {
           let time = moment(lineEntry.date, 'YYYY-MM-DD hh:mm:ss a');
-          // let handle = lineEntry.author_name;
-          // try {
-          //   handle = lineEntry.author_name.match(/\((.+)\)$/)[1];
-          // } catch (e) {}
 
           try {
-            // let firstWord = lineEntry.message.split(' ')[0];
-            // let isPast = tensify(firstWord).past === firstWord;
-            // let isParticiple = tensify(firstWord).past_participle === firstWord;
+            let combinedEntry = Object.assign({}, lineEntry, {
+              relativeTime: time.calendar()
+            });
 
-            let prefix = '';
-            // if (isPast) {
-            //   prefix = 'I';
-            // } else if (isParticiple) {
-            //   prefix = '';
-            // } else {
-            //   prefix = 'I committed:';
-            // }
-
-            // message for each commit
-            if(lineEntry.message && !lineEntry.message.toLowerCase().startsWith("merge"))
-            log(time.calendar() + ":", lineEntry.message);
+            log(
+              whimsy(
+                micromustache
+                  .render(flags.commitText, combinedEntry)
+                  // restore whimsy brackets
+                  .replace('[[', '{{')
+                  .replace(']]', '}}')
+              )
+            );
           } catch (e) {}
         });
       }
     });
 
     // Message when done
-    scroll('.');
+    if (flags.clearScreen) {
+      clearScreen();
+    } else {
+      scroll(flags.scrollText, 0, flags.scrollAmount);
+    }
+    // log('\x1B[2J')
     resolve();
   });
 });
